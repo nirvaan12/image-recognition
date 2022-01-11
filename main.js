d73d37f73e2567d5ae26349dcb449145329dd437
nirvaan12 <85714636+nirvaan12@users.noreply.github.com>
2022-01-11 18:59:10 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
 //https://teachablemachine.withgoogle.com/models/3zZc9IdeY/
+
+Webcam.set({
+    width:350,
+    height:300,
+    imageformat:'png',
+    png_quality:90
+});
+
+camera = document.getElementById("camera");
+
+Webcam.attach("#camera");
+
+function take_snapshot(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML="<img id='captureimg' src='"+data_uri+"'>";
+    });
+  
+}
+console.log('ml5 version: ',ml5.verison);
+classifier=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/3zZc9IdeY/model.json",modelLoaded);
