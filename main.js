ce8123da420567b16b08d7602154f8c8220d0b47
nirvaan12 <85714636+nirvaan12@users.noreply.github.com>
2022-01-13 18:49:55 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -19,3 +19,23 @@ function take_snapshot(){
 }
 console.log('ml5 version: ',ml5.verison);
 classifier=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/3zZc9IdeY/model.json",modelLoaded);
+
+function modelLoaded(){
+    console.log("Model is loaded!");
+}
+
+function check(){
+    img = document.getElementById("captureimg");
+    classifier.classify(img,gotResult);
+}
+
+function gotResult(error,results){
+    if(error){
+    console.error(error);
+    }
+    else{
+     console.log(results);
+     document.getElementById("result_object_name").innerHTML = results[0].label;
+     document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+    }
+}
